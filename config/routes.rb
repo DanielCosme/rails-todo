196e10318a1492b397bc0e5c9adb03948848b2b7
Daniel Cosme <cosmedaniel8@gmail.com>
2025-08-12 20:52:12 -0400

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :items
+  resources :projects
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  root "items#index"
+end
